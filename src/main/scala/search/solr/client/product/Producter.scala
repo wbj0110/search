39d82db4fca6e635364f929110e915a8f66c8b7b
soledede <wengbenjue@163.com>
2016-04-01 22:29:51 +0800

    s

--- src/main/scala/search/solr/client/product/Producter.scala
+++ src/main/scala/search/solr/client/product/Producter.scala
@@ -47,7 +47,7 @@ object Producter extends Logging with Configuration {
     * mergescloud_prod-null-null-396411
     * screencloud_prod-null-null-7406
     * screencloud_prod-null-null-8021
-    *mergescloud_prod-null-null-261468
+    *mergescloud_prod-null-null-260905
     * mergescloud_prod-null-null-2
     *
     * bin/kafka-console-producer.sh --broker-list 121.40.54.54:9092 --topic indexManagesTest
